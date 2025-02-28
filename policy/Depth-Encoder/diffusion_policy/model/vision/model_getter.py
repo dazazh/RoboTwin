b392a08022ae69c3f304aeb2c6566565ad731ab5
dazazh <berryibrahimmdy@gmail.com>
2025-03-01 00:53:56 +0800

    update

---
+++ policy/Depth-Encoder/diffusion_policy/model/vision/model_getter.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+
+def get_resnet(name, weights=None, **kwargs):
+    """
+    name: resnet18, resnet34, resnet50
+    weights: "IMAGENET1K_V1", "r3m"
+    """
+    # load r3m weights
+    if (weights == "r3m") or (weights == "R3M"):
+        return get_r3m(name=name, **kwargs)
+
+    func = getattr(torchvision.models, name)
+    resnet = func(weights=weights, **kwargs)
+    resnet.fc = torch.nn.Identity()
+
+    conv1 = resnet.conv1
+    # 修改为支持4通道输入
+    new_conv1 = torch.nn.Conv2d(4, conv1.out_channels, kernel_size=conv1.kernel_size,
+                                stride=conv1.stride, padding=conv1.padding, bias=False)
+    # 将修改后的卷积层替换原有的第一层卷积层
+    resnet.conv1 = new_conv1
+    return resnet
+    # resnet_new = torch.nn.Sequential(
+    #     resnet,
+    #     torch.nn.Linear(512, 128)
+    # )
+    # return resnet_new
+    return resnet
+
+def get_r3m(name, **kwargs):
+    """
+    name: resnet18, resnet34, resnet50
+    """
+    import r3m
+    r3m.device = 'cpu'
+    model = r3m.load_r3m(name)
+    r3m_model = model.module
+    resnet_model = r3m_model.convnet
+    resnet_model = resnet_model.to('cpu')
+    return resnet_model
